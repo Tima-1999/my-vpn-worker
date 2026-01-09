3c1d59e2a4fb264e17d51ce0dab52ce887e90976
Tima-1999 <tmsanly@sanly.tm>
2026-01-09 22:01:55 +0500

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,16 +1,15 @@
 export default {
   async fetch(request, env) {
-    const targetAddress = '83.217.9.70'; 
-    const targetPort = '10002'; // Porty 10002 etdik (Seniň kodyňdaky ýaly)
+    const targetAddress = '83.217.9.70'; // Seniň gizlemeli IP-ň
+    const url = new URL(request.url);
 
     if (request.headers.get('Upgrade') === 'websocket') {
-      // VpnJantit TLS ulanýar, şonuň üçin 'https' we '443' porty gerek bolup biler
-      // Emma köplenç 'http' we '10002' işleýär. Gel, ilki şu iň dogrusyny barlalyň:
-      return fetch(`http://${targetAddress}:${targetPort}${new URL(request.url).pathname}${new URL(request.url).search}`, {
+      // Ähli trafigi VpnJantit-e ugradýarys
+      return fetch(`https://${targetAddress}${url.pathname}${url.search}`, {
         headers: request.headers,
       });
     }
 
-    return new Response("Worker is active for Port 10002", { status: 200 });
+    return new Response("Bridge is active!", { status: 200 });
   }
 };
