7757bcd5b04c0729f9c65bd47cf4d2b165f69378
Tima-1999 <tmsanly@sanly.tm>
2026-01-09 21:31:55 +0500

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,15 +1,18 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
-    const targetAddress = '83.217.9.70'; // Seniň IP-ň
-    const targetPort = '1002';           // Seniň portuň
+    const targetAddress = '83.217.9.70'; 
+    const targetPort = '1002';
+
+    // Cloudflare bloklaryny aşmak üçin Header-leri goraýarys
+    const newHeaders = new Headers(request.headers);
+    newHeaders.set('Host', 'cf-vpn.tmsanly.workers.dev');
 
     if (request.headers.get('Upgrade') === 'websocket') {
       return fetch(`http://${targetAddress}:${targetPort}${url.pathname}${url.search}`, {
-        headers: request.headers,
-        redirect: 'follow',
+        headers: newHeaders,
       });
     }
-    return new Response("VPN Server is active!", { status: 200 });
+    return new Response("Sistem taze CDN bilen tayyar!", { status: 200 });
   }
 };
